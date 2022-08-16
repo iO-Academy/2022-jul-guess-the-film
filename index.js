9389ac2c39e971d6a75abc4db1f7bc1b4e896dde
josh-lew <joshlewisdev@gmail.com>
2022-08-16 09:29:38 +0100

    html tidied, IDs changed for modal in HTML CSS and JS

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+let modal = document.getElementById("instructionsModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+let btn = document.getElementById("instructionsModalBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.addEventListener('click', () => {
   modal.style.display = "block";
-}
+})
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.addEventListener('click', () => {
   modal.style.display = "none";
-}
+})
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
+window.addEventListener('click', (e) => {
+  if (e.target == modal) {
     modal.style.display = "none";
   }
-}
+})
