0c697240673430ca25751a93b796e38e57b69040
mikeycodingstuff <mikeyinguk@gmail.com>
2022-08-16 11:20:06 +0100

    removed line breaks in index.js

--- index.js
+++ index.js
@@ -2,17 +2,14 @@
 let modal = document.getElementById("instructionsModal");
 let btn = document.getElementById("instructionsModalBtn");
 let span = document.getElementsByClassName("close")[0];
-
 // When the user clicks on the button, open the modal
 btn.addEventListener('click', () => {
   modal.style.display = "block";
 })
-
 // When the user clicks on <span> (x), close the modal
 span.addEventListener('click', () => {
   modal.style.display = "none";
 })
-
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click', (e) => {
   if (e.target == modal) {
