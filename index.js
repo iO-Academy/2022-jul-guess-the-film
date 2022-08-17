14f1e7eb6cf474b04bbd86dca1a454e21d2d5d11
cr-walton <cxw507@gmail.com>
2022-08-17 09:58:15 +0100

    functionality of selecting films, choosing winner, displaying buttons, displaying quote and json file

--- index.js
+++ index.js
@@ -16,3 +16,40 @@ window.addEventListener('click', (e) => {
     modal.style.display = "none";
   }
 })
+
+fetch('quotes.json')
+    .then((data) => {
+        return data.json()
+    })
+
+    .then((apiResponse) => {
+      shuffleArray(apiResponse.films)
+      let threeFilms = apiResponse.films.slice(0, 3)
+      let winningFilmObject = threeFilms[0]
+      shuffleArray(threeFilms)
+      let p_text = document.createTextNode(winningFilmObject.quote)
+      document.querySelector('#quote').appendChild(p_text)
+      let p_tag = document.querySelector('#quote')
+      threeFilms.forEach((oneFilm) => {
+           let button_tag = document.createElement('button')
+           let button_text = document.createTextNode(oneFilm.title)
+           button_tag.appendChild(button_text)
+           document.querySelector('#btnContainer').appendChild(button_tag)
+           if(p_tag.textContent === oneFilm.quote) {
+            button_tag.dataset.winner = true
+        } else {
+            button_tag.dataset.winner = false
+        }
+    })
+})
+
+const shuffleArray = array => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+
+    return array;
+  }
